510fe8bc002235c8c77b880cfb78238dd2b84cd4
quang <quanglatoi002@gmail.com>
2023-08-24 17:49:39 +0700

    Coding Filter and pagination

--- server/controllers/product.js
+++ server/controllers/product.js
@@ -34,10 +34,9 @@ const getProducts = asyncHandler(async (req, res) => {
     excludeFields.forEach((el) => delete queries[el]);
     //Format
     let queryString = JSON.stringify(queries);
-    console.log(queryString);
     queryString = queryString.replace(/\b(gte|gt|lt|lte)\b/g, (el) => `$${el}`);
     const formattedQueries = JSON.parse(queryString);
-    console.log(formattedQueries);
+
     // price : {}
     //Filtering
     // chỉ cần 1 chữ tồn tại trong nd cần tìm thì nó sẽ hiện ra toàn bộ, "i" không phân biệt chữ hoa và thường.
@@ -49,11 +48,23 @@ const getProducts = asyncHandler(async (req, res) => {
     // quang, thai, van => [quang, thai, van] => quang thai van
     if (req.query.sort) {
         const sortBy = req.query.sort.split(",").join(" ");
-        console.log(sortBy);
         //sort(title firstname)
-
         queryCommand = queryCommand.sort(sortBy);
     }
+
+    //Field Limiting
+    if (req.query.fields) {
+        const fields = req.query.fields.split(",").join(" ");
+        console.log(fields);
+        queryCommand = queryCommand.select(fields);
+    }
+
+    //Pagination
+    // page=2&limit=10, 1-10 page 1, 11-20 page 2, 21-30 page 3
+    const page = +req.query.page * 1 || 1;
+    const limit = +req.query.limit * 1 || process.env.LIMIT_PRODUCTS;
+    const skip = (page - 1) * limit;
+    queryCommand.skip(skip).limit(limit);
     //Execute the query
     // tìm ra số lượng thỏa đk và số lượng sp gọi ra từ API
     try {
@@ -63,8 +74,8 @@ const getProducts = asyncHandler(async (req, res) => {
 
         return res.status(200).json({
             success: response ? true : false,
-            products: response ? response : "Could not get products",
             counts,
+            products: response ? response : "Could not get products",
         });
     } catch (err) {
         throw new Error(err.message);
@@ -99,10 +110,19 @@ const deleteProduct = asyncHandler(async (req, res) => {
         productData: deleteProduct ? deleteProduct : "Could not delete product",
     });
 });
+
+//RATINGS
+const ratings = asyncHandler((req, res) => {
+    // const { _id } = req.user;
+    // const { star, comment, pid } = req.body;
+    // if (!star || !pid) throw new Error("Missing input");
+});
+
 module.exports = {
     createProduct,
     getProduct,
     getProducts,
     updateProduct,
     deleteProduct,
+    ratings,
 };
--- server/routes/product.js
+++ server/routes/product.js
@@ -4,6 +4,7 @@ const { verifyAccessToken, isAdmin } = require("../middlewares/verifyToken");
 
 router.post("/", [verifyAccessToken, isAdmin], ctrlc.createProduct);
 router.get("/", ctrlc.getProducts);
+router.get("/ratings", verifyAccessToken, ctrlc.ratings);
 
 router.put("/:pid", [verifyAccessToken, isAdmin], ctrlc.updateProduct);
 router.delete("/:pid", [verifyAccessToken, isAdmin], ctrlc.deleteProduct);
